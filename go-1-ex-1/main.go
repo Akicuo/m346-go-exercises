c69f8b38c24b15d756dc5983242271ce3e1a491e
Akij <user.of.replit@gmail.com>
2025-11-03 14:28:59 +0100

    vsars

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,14 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Lyan"
+	lastName := "Labaguis"
+	dayOfBirth := 4
+	monthOfBirth := 4
+	yearOfBirth := 2008
+	numberOfSiblings := 1
+	heightInMeters := 1.75
+	zodiacSign := "Aries"
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
