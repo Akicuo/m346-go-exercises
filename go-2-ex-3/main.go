53ca9997f4d66ee9841c6b63d7bd835b66951d87
Akij <user.of.replit@gmail.com>
2025-11-17 15:11:00 +0100

    fix go2

--- go-2-ex-3/main.go
+++ go-2-ex-3/main.go
@@ -4,10 +4,18 @@ import "fmt"
 
 func main() {
 	// TODO: create a map called "modules"
-
+	modules := map[int]string{
+		104: "Datenmodell ",
+		117: "Datenbank",
+		346: "Cloud",
+	}
 	fmt.Println("Modul 104:", modules[104])
 	fmt.Println("Modul 117:", modules[117])
 	fmt.Println("Modul 346:", modules[346])
+	delete(modules, 104)
+	modules[104] = "DM V2" // replace the removed value
+	// now replace a value with antother
+	modules[117] = "DB V2" //?
 
 	// TODO: delete one
 	// TODO: add one
--- go-2-ex-4/main.go
+++ go-2-ex-4/main.go
@@ -1,8 +1,70 @@
 package main
 
+import "fmt"
+
+type Student struct {
+	FirstName string
+	LastName  string
+}
+
+type Class struct {
+	Students []Student
+}
+
+type Module struct {
+	Number  int
+	Classes []Class
+}
+
 func main() {
+	// 1. Klasse
+	class1 := Class{
+		Students: []Student{
+			{FirstName: "Anna", LastName: "Müller"},
+			{FirstName: "Ben", LastName: "Schmidt"},
+			{FirstName: "Clara", LastName: "Weber"},
+		},
+	}
+
+	// 2. Klasse
+	class2 := Class{
+		Students: []Student{
+			{FirstName: "David", LastName: "Fischer"},
+			{FirstName: "Emma", LastName: "Wagner"},
+			{FirstName: "Felix", LastName: "Becker"},
+		},
+	}
+
+	// 3. Klasse
+	modules := map[int]Module{
+		346: {
+			Number:  346,
+			Classes: []Class{class1, class2},
+		},
+		104: {
+			Number:  104,
+			Classes: []Class{class1},
+		},
+		117: {
+			Number:  117,
+			Classes: []Class{class2},
+		},
+	}
+
+	// Output all data
+	for moduleNumber, module := range modules {
+		fmt.Printf("Modul %d:\n", moduleNumber)
+		for i, class := range module.Classes {
+			fmt.Printf("  Klasse %d:\n", i+1)
+			for _, student := range class.Students {
+				fmt.Printf("    - %s %s\n", student.FirstName, student.LastName)
+			}
+		}
+		fmt.Println()
+	}
+
 	// TODO: declare a type for Student (with first and last name)
-	// TODO: declare a type for Class (consisting of multiple students)
+	// TODO: declare a type for Class (	consisting of multiple students)
 	// TODO: declare a map of modules being attended by multiple classes
 	// TODO: output everything using fmt.Println()
 }
